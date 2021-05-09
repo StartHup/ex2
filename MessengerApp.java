35e1b97e526f404f1525dedf345a7288cc0495ab
Jacob <RoonilWazzlib@users.noreply.github.com>
2021-05-09 11:09:53 +0300

    Create MessengerApp.java

---
+++ MessengerApp.java
@@ -0,0 +1,17 @@
+public class MessengerApp implements App{
+    String[] Messages;
+    //Needs much more planning
+
+    @Override
+    public void printMenu() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void exitApp() {
+        // TODO Auto-generated method stub
+
+    }
+
+} 
