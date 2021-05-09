16eba22734edd1eacfb149a8eaa337a8dc3ff299
Jacob <RoonilWazzlib@users.noreply.github.com>
2021-05-09 11:03:48 +0300

    Create CalendarApp.java

---
+++ CalendarApp.java
@@ -0,0 +1,22 @@
+public class CalendarApp implements App {
+    
+    //List of days with events/meetings
+    BusyDate[] calander;
+    
+    public Calendar(){
+        calander = new BusyDate[30];
+    }
+  
+    @Override
+    public void printMenu() {
+        // TODO Auto-generated method stub
+
+    }
+  
+    @Override
+    public void exitApp() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
