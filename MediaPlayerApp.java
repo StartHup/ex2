dfd033ae900571b4fa9abe2612fe113ea72ff73e
RoonilWazzlib <JacobLevitan@gmail.com>
2021-05-10 11:17:44 +0300

    Start building createNewMediaFile() function

--- MediaPlayerApp.java
+++ MediaPlayerApp.java
@@ -1,6 +1,76 @@
 import java.util.*; 
 public class MediaPlayerApp implements App{
+    
+    static Scanner s = new Scanner(System.in);
+    
+    //Linked list to store all the media
+    ArrayList<Media> mediaList;
+    
+    //Constructor
+    public MediaPlayerApp() {
+        mediaList = new ArrayList<Media>();
+    } 
+    
+    public void addNewMediaFile(Media mediaFile){
+        if (mediaFile != null){
+            mediaList.add(mediaFile);
+        }
+    }
+    
+    public void createNewMediaFile(){
+        
+        //Retrieve file type
+        System.out.println("Enter the type of the new file (video or audio): ");
+        String fileType = s.nextLine();
+        
+        //Check valid file type
+        
+        //Retrieve name of new file
+        System.out.println("Enter the name of the new file (case sensitive): ");
+        String fileName = s.nextLine();
+        
+        //Check name validity
+        if (fileName.length() < 1){
+            System.out.println("Improper name length! Must be at least 1 character.");
+            return;
+        }
+        
+        //Input new file length
+        System.out.println("Enter file length in seconds: ");
+        String number = s.nextLine();
 
+        //Check number validity
+        if (!(isStringDigits(number))){
+            System.out.println("Only digits allowed in file length!");
+            return;
+        }
+
+        //Create contact
+        Media contact = new Contact(name, number);
+        
+        //Add contact to phonebook
+        phoneBook.add(contact);
+        System.out.println("Contact " + contact + " added successfully");
+        return;
+    }
+    
+    public boolean isListEmpty(){
+        return mediaList.size() == 0;
+    }
+    
+    public boolean isStringDigits(String string){
+        try {
+            //If parseInt completes properly => string is entirely digits
+            Integer.parseInt(string);
+            return true;
+        }
+      
+        //If NumberFormatException caught => there is a nondigit somewhere
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+    
     @Override
     public void runApp(){
         Scanner s = new Scanner(System.in);
