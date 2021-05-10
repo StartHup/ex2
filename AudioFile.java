395d8e2838b3072b41fa8817eee5813df849e16f
RoonilWazzlib <JacobLevitan@gmail.com>
2021-05-10 11:28:25 +0300

    Implement getMediaType and executeFile

--- AudioFile.java
+++ AudioFile.java
@@ -1,3 +1,12 @@
 public class AudioFile extends Media {
     
+    public String getMediaType() {
+        return "Audio";
+    }
+    
+    public void executeFile() {
+        System.out.println("Audio file " + this.getName() + "playing for " 
+            + this.formatLength());
+    }
+    
 }
--- VideoFile.java
+++ VideoFile.java
@@ -1,7 +1,12 @@
 public class VideoFile extends Media {
+    
     public String getMediaType() {
         return "Video";
     }
     
+    public void executeFile() {
+        System.out.println("Video file " + this.getName() + "playing for " 
+            + this.formatLength());
+    }
     
 }
